8c73210d5711f6dbdae484a1d9784b93afd93195
Khydra <adrianlacueva@gmail.com>
2025-02-13 06:50:41 +0100

    resources

--- js/game/data/item/recipeData.js
+++ js/game/data/item/recipeData.js
@@ -164,6 +164,20 @@ export const sastreRecipeData = {
 			pluma_de_pio_rosa: 3
 		},
 	},
+	capa_pandawushuka: {
+		item: equipmentData['capa_pandawushuka'],
+		recipe: {
+			espina_de_champi_champ: 3,
+			hoja_de_girasol_salvaje: 1,
+		},
+	},
+	capa_de_champi_champ: {
+		item: equipmentData['capa_de_champi_champ'],
+		recipe: {
+			espina_de_champi_champ: 3,
+			cola_de_miauflan: 3
+		},
+	},
 	casco_de_chadalid: {
 		item: equipmentData['casco_de_chadalid'],
 		recipe: {
@@ -190,6 +204,153 @@ export const sastreRecipeData = {
 		recipe: {
 			baba_de_jalato: 3, 
 			pluma_de_pio_violeta: 3
+		},
+	},
+	champisombrero: {
+		item: equipmentData['champisombrero'],
+		recipe: {
+			espina_de_champi_champ: 3,
+			pluma_quimerica: 3
+		},
+	},
+	capucha_de_los_campos: {
+		item: equipmentData['capucha_de_los_campos'],
+		recipe: {
+			lengua_de_espanterrador: 4,
+			baba_de_rosa_demoniaca: 1
+		},
+	},
+	sombrero_campestre: {
+		item: equipmentData['sombrero_campestre'],
+		recipe: {
+			flor_del_diente_de_leon_diabolico: 4,
+			hierba_loca: 1
+		},
+	},
+	capa_elini: {
+		item: equipmentData['capa_elini'],
+		recipe: {
+			pluma_de_pio_amarillo: 3,
+			fluido_glacial: 1,
+		},
+	},
+	capa_uleto: {
+		item: equipmentData['capa_uleto'],
+		recipe: {
+			bronceador: 3,
+			baba_de_rosa_demoniaca: 1
+		},
+	},
+	capa_sulacorp: {
+		item: equipmentData['capa_sulacorp'],
+		recipe: {
+			espina_de_champi_champ: 3,
+			tabla_de_aglomerado: 2,
+		},
+	},
+	capa_de_rojez: {
+		item: equipmentData['capa_de_rojez'],
+		recipe: {
+			pluma_de_pio_amarillo: 3,
+			alogdon_ancestral: 1
+		},
+	},
+	capa_bilidad: {
+		item: equipmentData['capa_bilidad'],
+		recipe: {
+			petalo_de_rosa_demoniaca: 3,
+			baba_de_rosa_demoniaca: 1
+		},
+	},
+	korko_klako: {
+		item: equipmentData['korko_klako'],
+		recipe: {
+			semilla_de_girasol: 1,
+			concentrado_de_matorral: 1
+		},
+	},
+	bolso_de_campesino: {
+		item: equipmentData['bolso_de_campesino'],
+		recipe: {
+			hierba_loca: 1,
+			petalo_magico_de_girasol_hambriento: 1
+		},
+	},
+	capa_oscura: {
+		item: equipmentData['capa_oscura'],
+		recipe: {
+			colmillos_de_ratas: 3,
+			alogdon_ancestral: 1
+		},
+	},
+	bob_de_campesino: {
+		item: equipmentData['bob_de_campesino'],
+		recipe: {
+			lengua_de_espanterrador: 4,
+			petalo_magico_de_girasol_hambriento: 1
+		},
+	},
+	sombrero_de_jalato: {
+		item: equipmentData['sombrero_de_jalato'],
+		recipe: {
+			cuerno_de_jalatin: 4,
+			lana_de_jefe_de_guerra_jalato: 1
+		},
+	},
+	capa_jalosa: {
+		item: equipmentData['capa_jalosa'],
+		recipe: {
+			sangre_de_jalato: 1,
+			cuerno_de_jalatin: 4,
+		},
+	},
+	sombrero_del_hombre_oso: {
+		item: equipmentData['sombrero_del_hombre_oso'],
+		recipe: {
+			colmillo_de_jabali: 4,
+			pelos_de_draft: 1
+		},
+	},
+	capa_del_hombre_oso: {
+		item: equipmentData['capa_del_hombre_oso'],
+		recipe: {
+			pincho_de_prespic: 4,
+			tejido_de_jabali: 1,
+		},
+	},
+	sombrero_invisible: {
+		item: equipmentData['sombrero_invisible'],
+		recipe: {
+			piel_de_mob_laesponja: 3,
+			reliquia_de_incarnam: 1
+		},
+	},
+	capa_invisible: {
+		item: equipmentData['capa_invisible'],
+		recipe: {
+			petalo_magico_de_girasol_hambriento: 3,
+			reliquia_de_incarnam: 1
+		},
+	},
+	casco: {
+		item: equipmentData['casco'],
+		recipe: {
+			alogdon_ancestral: 1,
+			madera_carcomida: 1
+		},
+	},
+	alas_de_madera: {
+		item: equipmentData['alas_de_madera'],
+		recipe: {
+			concentrado_de_matorral: 2,
+			hilo_de_seda: 1
+		},
+	},
+	sombrero_de_paletoso: {
+		item: equipmentData['sombrero_de_paletoso'],
+		recipe: {
+			flor_del_diente_de_leon_diabolico: 3,
+			colmillos_de_ratas: 3
 		},
 	},
 }
--- js/game/data/item/resourceData.js
+++ js/game/data/item/resourceData.js
@@ -15,6 +15,13 @@ export const resourceData = {
 		type: 'resource',
 		sort: 'madera' 
 	},
+	madera_carcomida: {
+		key: 'madera_carcomida',
+		name: 'Madera carcomida',
+		image: './assets/images/items/resources/01-0001.png',
+		type: 'resource',
+		sort: 'madera' 
+	},
 	// CEREAL
 	trigo: {
 		key: 'trigo',
@@ -31,6 +38,20 @@ export const resourceData = {
 		type: 'resource',
 		sort: 'planta' 
 	},
+	lengua_de_espanterrador: {
+		key: 'lengua_de_espanterrador',
+		name: 'Lenga de espanterror',
+		image: './assets/images/items/resources/03-0001.png',
+		type: 'resource',
+		sort: 'planta' 
+	},
+	hierba_loca: {
+		key: 'hierba_loca',
+		name: 'Hierba loca',
+		image: './assets/images/items/resources/03-0002.png',
+		type: 'resource',
+		sort: 'planta' 
+	},
 	// ALEACION
 	ferrita: {
 		key: 'ferrita',
@@ -40,9 +61,9 @@ export const resourceData = {
 		sort: 'aleacion' 
 	},
 	// TABLA
-	tabla_de_fresno: {
-		key: 'tabla_de_fresno',
-		name: 'Tabla de fresno',
+	tabla_de_aglomerado: {
+		key: 'tabla_de_aglomerado',
+		name: 'Tabla de aglomerado',
 		image: './assets/images/items/resources/05-0000.png',
 		type: 'resource',
 		sort: 'tabla' 
@@ -68,6 +89,41 @@ export const resourceData = {
 		image: './assets/images/items/resources/06-0002.png',
 		type: 'resource',
 		sort: 'recurso' 
+	},
+	hilo_de_seda: {
+		key: 'hilo_de_seda',
+		name: 'Hilo de seda',
+		image: './assets/images/items/resources/06-0003.png',
+		type: 'resource',
+		sort: 'resource' 
+	},
+	bronceador: {
+		key: 'bronceador',
+		name: 'Bronceador',
+		image: './assets/images/items/resources/06-0004.png',
+		type: 'resource',
+		sort: 'resource' 
+	},
+	alogdon_ancestral: {
+		key: 'alogdon_ancestral',
+		name: 'Algdón ancestral',
+		image: './assets/images/items/resources/06-0005.png',
+		type: 'resource',
+		sort: 'resource' 
+	},
+	caracola_marina: {
+		key: 'caracola_marina',
+		name: 'Caracola marina',
+		image: './assets/images/items/resources/06-0006.png',
+		type: 'resource',
+		sort: 'resource' 
+	},
+	aletas_de_competicion: {
+		key: 'aletas_de_competicion',
+		name: 'Aletas de competición',
+		image: './assets/images/items/resources/06-0007.png',
+		type: 'resource',
+		sort: 'resource' 
 	},
 	// PLUMA
 	pluma_quimerica: {
@@ -126,12 +182,47 @@ export const resourceData = {
 		image: './assets/images/items/resources/08-0000.png',
 		type: 'resource',
 		sort: 'piel' 
+	},		
+	piel_de_mob_laesponja: {
+		key: 'piel_de_mob_laesponja',
+		name: 'Piel de Mob Laesponja',
+		image: './assets/images/items/resources/17-0000.png',
+		type: 'resource',
+		sort: 'piel' 
 	},
 	// FLOR
 	petalo_diafano: {
 		key: 'petalo_diafano',
 		name: 'Pétalo diáfano',
 		image: './assets/images/items/resources/09-0000.png',
+		type: 'resource',
+		sort: 'flor' 
+	},
+	petalo_de_rosa_demoniaca: {
+		key: 'petalo_de_rosa_demoniaca',
+		name: 'Pétalo de rosa demoníaca',
+		image: './assets/images/items/resources/09-0001.png',
+		type: 'resource',
+		sort: 'flor' 
+	},
+	flor_del_diente_de_leon_diabolico: {
+		key: 'flor_del_diente_de_leon_diabolico',
+		name: 'Flor de diente de león diabólico',
+		image: './assets/images/items/resources/09-0002.png',
+		type: 'resource',
+		sort: 'flor' 
+	},
+	petalo_magico_de_girasol_hambriento: {
+		key: 'petalo_magico_de_girasol_hambriento',
+		name: 'Pétalo mágico de girasol hambriento',
+		image: './assets/images/items/resources/09-0003.png',
+		type: 'resource',
+		sort: 'flor' 
+	},
+	hoja_de_girasol_salvaje: {
+		key: 'hoja_de_girasol_salvaje',
+		name: 'Hoja de girasol salvaje',
+		image: './assets/images/items/resources/09-0004.png',
 		type: 'resource',
 		sort: 'flor' 
 	},
@@ -142,6 +233,27 @@ export const resourceData = {
 		image: './assets/images/items/resources/10-0000.png',
 		type: 'resource',
 		sort: 'lana' 
+	},
+	lana_de_jefe_de_guerra_jalato: {
+		key: 'lana_de_jefe_de_guerra_jalato',
+		name: 'Lana de jefe de guerra jalató',
+		image: './assets/images/items/resources/10-0001.png',
+		type: 'resource',
+		sort: 'lana' 
+	},
+	lana_de_jalato: {
+		key: 'lana_de_jalato',
+		name: 'Lana de jalató',
+		image: './assets/images/items/resources/10-0002.png',
+		type: 'resource',
+		sort: 'lana' 
+	},
+	lana_de_jalatin_negro: {
+		key: 'lana_de_jalatin_negro',
+		name: 'Lana de jalatín negro',
+		image: './assets/images/items/resources/10-0003.png',
+		type: 'resource',
+		sort: 'resoulanarce' 
 	},
 	// POLVO
 	cenizas_eternas: {
@@ -173,11 +285,39 @@ export const resourceData = {
 		type: 'resource',
 		sort: 'liquido' 
 	},
+	sangre_de_jalato: {
+		key: 'sangre_de_jalato',
+		name: 'Sangre de jalató',
+		image: './assets/images/items/resources/12-0002.png',
+		type: 'resource',
+		sort: 'liquido' 
+	},
+	fluido_glacial: {
+		key: 'fluido_glacial',
+		name: 'Fluido glacial',
+		image: './assets/images/items/resources/12-0003.png',
+		type: 'resource',
+		sort: 'liquido' 
+	},
+	baba_de_rosa_demoniaca: {
+		key: 'baba_de_rosa_demoniaca',
+		name: 'Baba de rosa demoníaca',
+		image: './assets/images/items/resources/12-0004.png',
+		type: 'resource',
+		sort: 'liquido' 
+	},
 	// HONGO
 	volva_de_amenita: {
 		key: 'volva_de_amenita',
 		name: 'Volva de amenita',
 		image: './assets/images/items/resources/13-0000.png',
+		type: 'resource',
+		sort: 'hongo' 
+	},
+	espina_de_champi_champ: {
+		key: 'espina_de_champi_champ',
+		name: 'Espina de champi champ',
+		image: './assets/images/items/resources/13-0001.png',
 		type: 'resource',
 		sort: 'hongo' 
 	},
@@ -204,6 +344,34 @@ export const resourceData = {
 		type: 'resource',
 		sort: 'hueso' 
 	},
+	colmillo_de_jabali: {
+		key: 'colmillo_de_jabali',
+		name: 'Colmillo de jabalí',
+		image: './assets/images/items/resources/15-0002.png',
+		type: 'resource',
+		sort: 'hueso' 
+	},
+	pincho_de_prespic: {
+		key: 'pincho_de_prespic',
+		name: 'Pincho de prespic',
+		image: './assets/images/items/resources/15-0003.png',
+		type: 'resource',
+		sort: 'hueso' 
+	},
+	cuerno_de_jalatin: {
+		key: 'cuerno_de_jalatin',
+		name: 'Cuerno de jalatín',
+		image: './assets/images/items/resources/15-0004.png',
+		type: 'resource',
+		sort: 'hueso' 
+	},
+	colmillos_de_ratas: {
+		key: 'colmillos_de_ratas',
+		name: 'Colmillos de ratas',
+		image: './assets/images/items/resources/15-0005.png',
+		type: 'resource',
+		sort: 'hueso' 
+	},
 	// PATA
 	pata_de_arakna: {
 		key: 'pata_de_arakna',
@@ -219,5 +387,59 @@ export const resourceData = {
 		image: './assets/images/items/resources/17-0000.png',
 		type: 'resource',
 		sort: 'pelos' 
-	}
+	},
+	pelos_de_darit: {
+		key: 'pelos_de_darit',
+		name: 'Pelos de Darit',
+		image: './assets/images/items/resources/17-0001.png',
+		type: 'resource',
+		sort: 'pelos' 
+	},
+	cejas_de_mosquito: {
+		key: 'cejas_de_mosquito',
+		name: 'Cejas de mosquito',
+		image: './assets/images/items/resources/17-0002.png',
+		type: 'resource',
+		sort: 'pelos' 
+	},
+	// CONCENTRADO
+	concentrado_de_matorral: {
+		key: 'concentrado_de_matorral',
+		name: 'Concentrado de matorral',
+		image: './assets/images/items/resources/18-0000.png',
+		type: 'resource',
+		sort: 'concentrado' 
+	},
+	// TEJIDO
+	tejido_de_jabali: {
+		key: 'tejido_de_jabali',
+		name: 'Tejido de jabalí',
+		image: './assets/images/items/resources/19-0000.png',
+		type: 'resource',
+		sort: 'teijo' 
+	},
+	// SEMILLA
+	semilla_de_girasol: {
+		key: 'semilla_de_girasol',
+		name: 'Semilla de girasol',
+		image: './assets/images/items/resources/20-0000.png',
+		type: 'resource',
+		sort: 'semilla' 
+	},
+	semilla_de_sesamo: {
+		key: 'semilla_de_sesamo',
+		name: 'Semillas de sésamo',
+		image: './assets/images/items/resources/20-0001.png',
+		type: 'resource',
+		sort: 'semilla' 
+	},
+
+
 }
+
+
+
+
+
+
+
